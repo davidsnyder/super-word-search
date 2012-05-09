6d51eff1d5e40ffe265470a9a60baab17ac4cfa7
David Snyder <dsnyder90@gmail.com>
2012-05-08 22:11:32 -0500

    initial implementation

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject super_word_search "1.0.0-SNAPSHOT"
+  :description "Super Word Search"
+  :dependencies [[org.clojure/clojure "1.2.1"] [org.clojure/clojure-contrib "1.2.0"]])
---
+++ test/super_word_search/test/core.clj
@@ -0,0 +1,6 @@
+(ns super_word_search.test.core
+  (:use [super_word_search.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
