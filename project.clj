55fbe5160a2c69fb3eeb4af9ed1ac71e06656f69
David Snyder <dsnyder90@gmail.com>
2012-05-09 04:16:48 -0500

    added input parser and -main method

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject super_word_search "1.0.0-SNAPSHOT"
+(defproject super-word-search "1.0.0-SNAPSHOT"
+  :main super-word-search.core  
   :description "Super Word Search"
   :dependencies [[org.clojure/clojure "1.2.1"] [org.clojure/clojure-contrib "1.2.0"]])
--- src/super_word_search/core.clj
+++ src/super_word_search/core.clj
@@ -1,14 +1,8 @@
-(ns super-word-search.core (:use [clojure.contrib.seq-utils :only [indexed]]))
+(ns super-word-search.core
+  (use [clojure.contrib.seq-utils :only [indexed]]))
 
 ;; 8 direction vectors, up, down, up-right, down-left, etc.
-(def dirs {:l [0,-1] 
-           :r [0,1]
-           :d [1,0]
-           :u [-1,0]
-           :ul [-1,-1]
-           :ur [-1,1]
-           :dl [1,-1]
-           :dr [1,1]})
+(def dirs {:l [0,-1] :r [0,1] :d [1,0] :u [-1,0] :ul [-1,-1] :ur [-1,1] :dl [1,-1] :dr [1,1]})
 
 ;; ======= HELPER FUNCS ==========
 
@@ -28,9 +22,11 @@
 ;; otherwise returns the new coordinate
 ;; (try-wrap '(-1 2) 3 3 true) => '(2 2)
 ;; (try-wrap '(-1 2) 3 3 false) => false
-(defn try-wrap [coord n m wrap-mode]
-  (let [wrapped-coord (wrap-coord coord n m)]
-    (if wrap-mode wrapped-coord (and (= coord wrapped-coord) coord))))
+(defn try-wrap [coord board]
+  (let [wrapped-coord (wrap-coord coord (board :n) (board :m))]
+    (if (board :wrap-mode)
+      wrapped-coord
+      (and (= coord wrapped-coord) coord))))
 
 ;; ======== LOOKUP TABLE ROUTINES ========  
 
@@ -45,34 +41,48 @@
 ;; Builds a map from a seq of [k v] pairs.  Values are appended to a list to
 ;; allow for duplicate key entries in @seq-pairs
 ;;{I ([2 2]), H ([2 1]), G ([2 0]), F ([1 2]), E ([1 1]), D ([1 0] [0 2]), B ([0 1]), A ([0 0])}
-(defn pairs-to-map [seq-pairs] (reduce (fn [m [k v]] (assoc m k (conj (m k) (seq v))))
-                                       {} seq-pairs))
+(defn pairs-to-map [seq-pairs] (reduce (fn [m [k v]] (assoc m k (conj (m k) (seq v)))) {} seq-pairs))
 
 ;; Map of coordinate lists for each letter in word search grid
 ;; (letter-coord-table 'D) => ((1 0) (0 2))
-(def letter-coord-table (pairs-to-map (seq-pairs grid)))
+(defn letter-coord-table [grid] (pairs-to-map (seq-pairs grid))) ;; FIXME: memoize
 
 ;; ============= SOLVE ===============
 
-(def grid [[\A \B \C] [\D \E \F] [\G \H \I]])
-
-;; board dimensions: [n m] -> [rows columns]
-(def board [3 3])
-
-(def input ["FED" "CAB" "GAD" "BID" "HIGH"])
-
-;;FIXME: Doesn't work for single letter words, no way to know if you've overlapped yourself
-(defn solve-word [word n m wrap-mode]
+(defn solve-word [word board]
   (or (first (filter (fn [[coord dir]] ;filter is lazy, so first will stop at the first solution
                        (let [start-coord coord] ;hold on to the coord of the first letter
-                         (loop [word (rest word), cur-coord coord, dir dir, n n, m m, wrap-mode wrap-mode]
+                         (loop [word (rest word), cur-coord coord, dir dir, board board] 
                            (if (and (empty? word) (not= start-coord cur-coord)) ; base case: we're out of letters and we haven't overlapped start-coord
                              [start-coord cur-coord] ; yield solution
-                             (let [wrapped-coord (try-wrap (add-coords cur-coord dir), n, m, wrap-mode)] ;take a step in the direction of @dir@                               
-                               (if (and wrapped-coord  
-                                        (= (get-element wrapped-coord grid)
-                                           (first word))) ; step was valid
-                                 (recur (rest word),wrapped-coord,dir,n,m,wrap-mode) ;continue searching in direction @dir@ for the rest of the letters
-                                 false)))))) ; step was not valid
-                     (for [coord (letter-coord-table (first word)), dir (vals dirs)] [coord dir]))) ;enumeration of 8 directions from each start-coord
-      "NOT FOUND")) 
+                             (let [wrapped-coord (try-wrap (add-coords cur-coord dir) board)] ;take a step in the direction of @dir@                               
+                               (if (and wrapped-coord  ;step is valid
+                                        (= (get-element wrapped-coord (board :grid))
+                                                        (first word))) ;the letter in this
+                                        ;direction matches the next letter
+                                        ;of word
+                                        (recur (rest word),wrapped-coord,dir,board) ;continue searching in direction @dir@ for the rest of the letters
+                                        false)))))) ; step was not valid
+                       (for [coord ((letter-coord-table (board :grid)) (first word)), dir (vals dirs)] [coord dir]))) ;enumeration of 8 directions from each start-coord
+             "NOT FOUND"))
+
+;; ========== MAIN =========
+
+(defn parse-input [input]
+  (reduce (fn [map line]
+            (cond (re-matches #"(\d)\s(\d)" line) ; set the grid dimensions
+                  (let [[_ n m] (re-matches #"(\d)\s(\d)" line)] (into map [[:n (read-string n)] [:m (read-string m)],[:grid-count (read-string n)]])) 
+                  (re-matches #"\d" line) ;set the number of search words
+                  (let [term-count (read-string (re-matches #"\d" line))] (assoc map :tc term-count)) 
+                  (re-matches #"NO_WRAP|WRAP" line) ;set the wrap-mode
+                  (let [wrap-mode (re-matches #"NO_WRAP|WRAP" line)] (assoc map :wrap-mode (= "WRAP" wrap-mode)))
+                  (> (:grid-count map) 0 ) ;append the grid row and decrement the counter
+                  (into map [[:grid-count (dec (:grid-count map))] [:grid (conj (or (:grid map) []) line)]]) 
+                  (> (:tc map) 0) ;append the search term and decrement the counter
+                  (into map [[:tc (dec (:tc map))] [:search-terms (conj (or (:search-terms map) []) line)]]))) 
+          {}
+          input))
+
+(defn -main [& args]
+  (let [board (parse-input (line-seq (java.io.BufferedReader. *in*)))]
+    (map #(solve-word %1 board) (board :search-terms))))
